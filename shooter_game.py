d4d2fad3603a1dac6c2abecdff0b283f7c1fed9d
thienhuu2015 <thienhuu.ln@gmail.com>
2025-11-28 19:12:21 +0700

    lesson 3

--- shooter_game.py
+++ shooter_game.py
@@ -4,6 +4,7 @@ from pygame import *
 from random import randint
 speed = 10
 lost = 0
+score = 0
 font.init()
 style = font.Font(None, 36)
 class GameSprite(sprite.Sprite):
@@ -28,7 +29,7 @@ class Player(GameSprite):
         if keys_pressed[K_DOWN] and self.rect.y < 395:
             self.rect.y += speed
     def fire(self):
-        bullet = Bullet("bullet.png",self.rect.centerx,self.rect.top,5,10,10)
+        bullet = Bullet("Capture.PNG",self.rect.centerx,self.rect.top,5,10,10)
         bullets.add(bullet)
 class Enemy(GameSprite):
     def update(self):
@@ -43,15 +44,15 @@ class Bullet(GameSprite):
         self.rect.y -= self.speed
         if self.rect.y <= 0:
             self.kill()
-rocket = Player("rocket.png",100,500,1,100,70)
+rocket = Player("Flamethrower.webp",100,500,1,100,70)
 enemies = sprite.Group()
 bullets = sprite.Group()
 for i in range(0,6):
-    enemy = Enemy("ufo.png",randint(0,500),0,randint(1,3),70,70)
+    enemy = Enemy("StriderSkibidi.webp",randint(0,500),0,randint(1,3),70,70)
     enemies.add(enemy)
 window = display.set_mode((500, 700))
 display.set_caption("shooter")
-background = transform.scale(image.load("galaxy.jpg"), (500, 700))
+background = transform.scale(image.load("hahha.PNG"), (500, 700))
 
 
 #parameters of the image sprite
@@ -65,6 +66,9 @@ mixer.music.play()
 finish = False
 
 while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
     while not finish:
         for e in event.get():
             if e.type == QUIT:
@@ -81,11 +85,35 @@ while run:
         enemies.update()
         bullets.draw(window)
         bullets.update()
+        collides = sprite.groupcollide(enemies,bullets,True,False)
+        for collide in collides:
+            score += 1
+            enemy = Enemy("StriderSkibidi.webp",randint(0,500),0,randint(1,3),70,70)
+            enemies.add(enemy)
 
         text_lose = style.render(
             "Missed:" + str(lost), 1,(255,255,255)
         )
         window.blit(text_lose,(10,20))
+        text_score = style.render(
+            "score:" + str(score), 1,(255,255,255)
+        )
+        window.blit(text_score,(10,40))
+        if sprite.spritecollide(rocket,enemies,False) or lost >= 10:
+            finish = True
+            text_fail = style.render(
+            "You lose hahaha >:)", 1,(255,0,0)
+        )
+            window.blit(text_fail,(300,300))
+        if score >= 100:
+            finish = True
+            text_win = style.render(
+            "You win but you is noob because it's easy >:(" , 1,(0,0,255)
+        )
+            window.blit(text_win,(300,300))
+
         display.update()
         clock.tick(FPS)
+    display.update()
+    clock.tick(FPS)
 
